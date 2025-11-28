60120109547879676442e5c481211dce1abda278
Rishabh-Dev-CSE <rishabhshakya9336@gmail.com>
2025-11-28 22:55:05 +0530

    Control panel route

--- frontend/src/admin_dashbord/control_panel/ControlPanelRoute.jsx
+++ frontend/src/admin_dashbord/control_panel/ControlPanelRoute.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import CreateUser from "./create_user/CreateUser";
+import UserList from "./user_list/UsersList";
 
 const ControlPanel = () => {
   const [activeTab, setActiveTab] = useState("create");
@@ -7,13 +8,23 @@ const ControlPanel = () => {
   const renderContent = () => {
     switch (activeTab) {
       case "create":
-        return(
-          <CreateUser />
+        return (
+          <>
+            
+            <CreateUser />
+          </>
         );
-      case "update":
-        return <div>Update User Form Area</div>;
       case "view":
-        return <div>View User Details Area</div>;
+        return (
+          <>
+            <div className="flex justify-center">
+              <h1 className="text-[3.4vh] font-semibold">
+                Users List
+              </h1>
+            </div>
+            <UserList />
+          </>
+        )
       default:
         return null;
     }
@@ -24,33 +35,20 @@ const ControlPanel = () => {
       {/* Top Buttons */}
       <div className="flex gap-4 border p-3 rounded-lg bg-white shadow-sm">
         <button
-          className={`px-4 py-2 rounded-lg border ${
-            activeTab === "create"
-              ? "bg-blue-600 text-white border-blue-600"
-              : "bg-gray-100 hover:bg-gray-200"
-          }`}
+          className={`px-4 py-2 rounded-lg border ${activeTab === "create"
+            ? "bg-blue-600 text-white border-blue-600"
+            : "bg-gray-100 hover:bg-gray-200"
+            }`}
           onClick={() => setActiveTab("create")}
         >
           Create User
         </button>
 
         <button
-          className={`px-4 py-2 rounded-lg border ${
-            activeTab === "update"
-              ? "bg-green-600 text-white border-green-600"
-              : "bg-gray-100 hover:bg-gray-200"
-          }`}
-          onClick={() => setActiveTab("update")}
-        >
-          Update User
-        </button>
-
-        <button
-          className={`px-4 py-2 rounded-lg border ${
-            activeTab === "view"
-              ? "bg-gray-700 text-white border-gray-700"
-              : "bg-gray-100 hover:bg-gray-200"
-          }`}
+          className={`px-4 py-2 rounded-lg border ${activeTab === "view"
+            ? "bg-gray-700 text-white border-gray-700"
+            : "bg-gray-100 hover:bg-gray-200"
+            }`}
           onClick={() => setActiveTab("view")}
         >
           View User
@@ -61,7 +59,7 @@ const ControlPanel = () => {
       <div className="w-full mt-5 p-5 border rounded-xl bg-gray-50 min-h-[250px] shadow-inner">
         {renderContent()}
       </div>
-      
+
     </div>
   );
 };
